f272dcc9f176125a27fec2fea7e1ece7dbb0d1ac
Mysterious Ben <datascience@tuta.io>
2020-10-03 21:58:26 +0700

    refactor: simplify github version control

--- scripts/version.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-from pathlib import Path
-
-if os.getenv('GITHUB_REF', '').startswith('refs/tags/'):
-    print(os.environ['GITHUB_REF'].split('/')[-1])
-else:
-    p = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], cwd=Path(__file__).parent.parent, stdout=subprocess.PIPE)
-    print(p.stdout.decode('utf-8'))
